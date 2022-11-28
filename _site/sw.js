9aba38aecd8c21aa5296190d478f9c1052abc23c
virtuallyNew <jason.f.harvey@gmail.com>
2022-11-28 14:45:44 -0500

    Index updates

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221128.141847';
+const cacheName = 'chirpy-20221128.143405';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
