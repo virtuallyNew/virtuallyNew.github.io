c368033af554d0fd143145759969b825ba0d8cea
virtuallyNew <jason.f.harvey@gmail.com>
2022-11-29 13:39:59 -0500

    updated about and rerun

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221129.120337';
+const cacheName = 'chirpy-20221129.133933';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
