c6a81db747bfc670ab8ff6cc310a35a5f9480422
virtuallyNew <jason.f.harvey@gmail.com>
2023-07-30 16:48:45 -0400

    jekyll

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230730.163222';
+const cacheName = 'chirpy-20230730.164614';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
