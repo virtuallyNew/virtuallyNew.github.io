8b7f1276dd38851f924735b010c6fe0179ffe0b4
virtuallyNew <jason.f.harvey@gmail.com>
2023-07-30 17:11:08 -0400

    updated field

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230730.164614';
+const cacheName = 'chirpy-20230730.171021';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
