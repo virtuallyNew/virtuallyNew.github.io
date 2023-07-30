1078853448afb33c970aa7046dae5f0cac078851
virtuallyNew <jason.f.harvey@gmail.com>
2023-07-30 10:04:28 -0400

    bundler

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230730.094711';
+const cacheName = 'chirpy-20230730.100402';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
