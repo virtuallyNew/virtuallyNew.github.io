5874766ed547e267eb55d98ea48ab96c0cf1c064
virtuallyNew <jason.f.harvey@gmail.com>
2023-07-30 16:33:06 -0400

    gem update --system

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230730.100402';
+const cacheName = 'chirpy-20230730.163222';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
