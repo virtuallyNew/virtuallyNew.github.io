7730aa22f568a32a3a47974bb7f3b456f967d9b2
virtuallyNew <jason.f.harvey@gmail.com>
2023-07-30 20:19:29 -0400

    local noko update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230730.201138';
+const cacheName = 'chirpy-20230730.201840';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
